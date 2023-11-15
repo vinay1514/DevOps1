9bc4b9c9aa23d0011a7013f9a7f1b9bf4cce42e6
vinaykumarpa <vinaykumarpa@10.4.33.14>
2023-11-15 12:36:21 +0530

    Commit1

---
+++ src/main/java/com/infinite/HelloController1.java
@@ -0,0 +1,11 @@
+package com.infinite;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class HelloController1 {
+@RequestMapping("/hello1")
+	public String display()
+	{
+		return "viewpage1";
+	}	
+}
